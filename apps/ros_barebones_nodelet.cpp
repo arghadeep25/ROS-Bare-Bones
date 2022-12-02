768453384fecdf83777d3bafdee5a90eab611d80
Arghadeep <arghadeep.mazumder@teraki.com>
2022-12-02 12:55:19 +0100

    message format and app modified

--- apps/ros_barebones_nodelet.cpp
+++ apps/ros_barebones_nodelet.cpp
@@ -1,8 +1,15 @@
+/************************************
+* @file ros_barebones_nodelet.cpp
+* @details ROS Bare Bones Nodelet
+* @author Arghadeep Mazumder
+* @version 1.0.0
+* @copyright -
+************************************/
+
 #include <iostream>
 #include <nodelet/nodelet.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
-#include <std_msgs/String.h>
 
 #include <utils.hpp>
 
@@ -10,41 +17,44 @@ namespace ros_bare_bones {
 class ROSBareBonesNodelet : public nodelet::Nodelet {
 
 public:
+  // Default Constructor
   ROSBareBonesNodelet() = default;
+public:
+  // Default Destructor
   ~ROSBareBonesNodelet() override = default;
 
 private:
   /**
-   *
+   * @brief init function
    */
   void onInit() override {
-    nh = getNodeHandle();
-    mt_nh = getMTNodeHandle();
-    private_nh = getPrivateNodeHandle();
-
-    pub = nh.advertise<std_msgs::String>("", 10);
+    // ROS nodelet info with function name
+    NODELET_INFO("ROSBareBonesNodelet - %s", __FUNCTION__);
     message_callback();
   }
 
 private:
+  /**
+   * @brief Message Callback function
+   */
   void message_callback() {
-    std_msgs::String message;
-    std::stringstream sstream;
-    sstream << "hello world";
-    message.data = sstream.str();
-    ROS_INFO("%s", message.data.c_str());
-    pub.publish(message);
+    // ROS nodelet info with function name
+    NODELET_INFO("ROSBareBonesNodelet - %s", __FUNCTION__);
+    // Setting the frequency at which message will be displayed
+    ros::Rate rate(2);
+    // Infinite Loop where the message will be printed
+    while(ros::ok()){
+      // Printing the message
+      MESSAGE("Counter: ",counter);
+      // Using ROS sleep function
+      rate.sleep();
+      // Incrementing the counter
+      counter++;
+    }
   }
 
 private:
-  // ROS Node Handler
-  ros::NodeHandle nh;
-  ros::NodeHandle mt_nh;
-  ros::NodeHandle private_nh;
-
-  // Subscriber and Publisher
-  ros::Subscriber sub;
-  ros::Publisher pub;
+  int counter = 0;
 };
 PLUGINLIB_EXPORT_CLASS(ros_bare_bones::ROSBareBonesNodelet, nodelet::Nodelet)
 } // namespace ros_bare_bones
