7908f2476222948d77ed7201b06c087f82250b77
Arghadeep <arghadeep.mazumder@teraki.com>
2022-12-06 17:50:33 +0100

    docker build file modified

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-set -e
+#set -e
+#
+#PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
+#DOCKER_ROOT=${PROJECT_ROOT}/"/docker"
+#echo "Project Root: $PROJECT_ROOT"
+#
+#echo "Building Docker image..."
+#docker build --tag rosbarebones ${DOCKER_ROOT}
 
-PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
-DOCKER_ROOT=${PROJECT_ROOT}/"/docker"
-echo "Project Root: $PROJECT_ROOT"
-
-echo "Building Docker image..."
-docker build --tag rosbarebones ${DOCKER_ROOT}
+docker build --tag rosbarebones -f melodic/Dockerfile ..
