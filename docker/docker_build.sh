e8d6ee2a51d19609f47211ceff868b241b46b44b
Arghadeep Mazumder <arghadeep.mazumder@teraki.com>
2022-12-07 15:40:17 +0100

    noetic tested

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-#set -e
-#
-#PROJECT_ROOT="$(cd "$(dirname "$0")"; cd ..; pwd)"
-#DOCKER_ROOT=${PROJECT_ROOT}/"/docker"
-#echo "Project Root: $PROJECT_ROOT"
-#
-#echo "Building Docker image..."
-#docker build --tag rosbarebones ${DOCKER_ROOT}
-
-docker build --tag rosbarebones -f melodic/Dockerfile ..
+docker build --tag rosbarebones -f noetic/Dockerfile ..
